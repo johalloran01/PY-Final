b7fce115cafc61af3c4bda243857ed3802bb9652
khanhquocng2801 <102694034+khanhquocng2801@users.noreply.github.com>
2022-03-31 12:58:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 import pygame
-#initialize the pygame lib
 pygame.init()
 
-def template(): #game window
-    #set the window of game pixel x pixel can be change depends on the map
+
+def screensetting():
+    #screen window setting pixel x pixel
     screen = pygame.display.set_mode((800,600))
-    #icon and title setting for application
-    icon = pygame.image.load('put your file name here')
+    #application NAME and ICON
+    pygame.display.set_caption('DEMO')
+    icon = pygame.image.load('sword.png')
     pygame.display.set_icon(icon)
-    pygame.display.set_caption("NAME OF THE GAME")
 
-#GAME LOOP
-def looper():   #creating a function for future use
-    running = True #putting the game in infinite loop
+
+def loop():
+    running = True
     while running:
-        for event in pygame.event.get():   #running all event in game
-            if event.type == pygame.quit(): #clicking cross on application window will close the app
-                running = False #break out of loop and close the program
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running=False
 
-template()
-looper()
-
-
+screensetting()
+loop()
 
 
