5ec45cfb710b1dddfe15e8a72c6f90bc056cc160
trimblejs4 <102620490+trimblejs4@users.noreply.github.com>
2022-04-07 11:00:40 -0400

    Setup window

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pygame
 pygame.init()
 #initialize pygame lib (must have)
-
+#used to setup frame rate
+clock = pygame.time.Clock()
 
 def screensetting():
     #screen window setting pixel x pixel
     screen = pygame.display.set_mode((800,600))
     #application NAME and ICON
-    pygame.display.set_caption('DEMO')
-    icon = pygame.image.load('sword.png')
+    pygame.display.set_caption('Melden Ting')
+    #creates icon for window based on png file
+    icon = pygame.image.load('dungeon-light.png')
     pygame.display.set_icon(icon)
 
 
@@ -18,6 +20,9 @@ def loop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running=False
+        pygame.display.update()
+        clock.tick(60)
+
 
 screensetting()
 loop()
