3507b9f2ef772dba3023b7c1abe4b02361af900e
khanhquocng2801 <102694034+khanhquocng2801@users.noreply.github.com>
2022-04-11 15:20:30 -0400

    Delete plains.tsx

--- Maps/plains.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="plains" tilewidth="16" tileheight="16" tilecount="72" columns="6">
- <image source="../images/sprites/tilesets/plains.png" width="96" height="192"/>
-</tileset>
