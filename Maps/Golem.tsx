2f36d954800c4ee134fe7a6448cc744918db2d63
khanhquocng2801 <102694034+khanhquocng2801@users.noreply.github.com>
2022-04-25 16:36:58 -0400

    Delete Maps directory

--- Maps/Golem.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="Character_sheet" tilewidth="16" tileheight="16" tilecount="3844" columns="62">
- <image source="C:/Users/khanh/Downloads/Mecha-stone Golem 0.1/Mecha-stone Golem 0.1/PNG sheet/Character_sheet.png" width="1000" height="1000"/>
-</tileset>
--- Maps/TX Props.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="TX Props" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="C:/Users/khanh/Downloads/Pixel Art Top Down - Basic/Texture/TX Props.png" width="512" height="512"/>
-</tileset>
